4f8ffa904b7924d242b361ba6d1064c3cc87c520
Mohamed Elashri <s-mohamed.elashri@zewailcity.edu.eg>
2022-04-05 18:07:51 +0000

    increase snippet content and title length

--- app/models/snippet_file.rb
+++ app/models/snippet_file.rb
@@ -3,8 +3,8 @@ class SnippetFile < ApplicationRecord
 
   counter_culture :snippet
 
-  validates :title, presence: true, length: { maximum: 40 }
-  validates :content, presence: true, length: { maximum: 20000 }
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :content, presence: true, length: { maximum: 200000 }
   validates :language, presence: true, inclusion: { in: Editor.languages.keys.map(&:to_s) }
   validates :tabs, numericality: { only_integer: true }, inclusion: { in: [2, 4, 8] }
 end
